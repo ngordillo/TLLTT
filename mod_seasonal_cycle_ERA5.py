aa6b19ec3baefa428089bce2b9eb3a6f7f29e567
ngordillo <nicojg@rams.colostate.edu>
2023-06-20 14:05:56 -0600

    ERA5 updates

--- mod_seasonal_cycle_ERA5.py
+++ mod_seasonal_cycle_ERA5.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
 from icecream import ic
-from sklearn.linear_model import LinearRegression
+from numpy.polynomial import polynomial
 import scipy.io as sio
 import pandas as pd
 import cartopy.crs as ccrs
@@ -30,70 +30,177 @@ def running_mean(x, N):
     cumsum = np.cumsum(np.insert(x, 0, np.zeros(x.shape[2]), axis = 0), axis =0)
     return (cumsum[N:] - cumsum[:-N]) / float(N)
 
+def find_nearest_index(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
 
 #load_dir = "~/Documents/Work/2021_Fall_IAI/Data/ERA5_Data/Precip/"
-load_dir = "/ourdisk/hpc/ai2es/nicojg/TLLTT/data/"
+load_dir = "/barnes-scratch/nicojg/"
 
-filename =  '4back_era5_daily_mjo_noleap_precip.nc'#4back_era5_daily_noleap_precip.nc'#era5_daily_2mtemp.nc'#mjo_precip_local.nc'#'mjo_200_precip.nc'#small_2mtemp.nc'#tropic_200_z500.nc'
+filename =   '4back_era5_daily_precip_remap.nc' #'era_no1959_2mtemp_remap.nc'#'4back_era5_daily_precip_remap.nc' #'era5_daily_2mtemp_remap.nc' #'era5_daily_precip_remap.nc' #'4back_era5_daily_mjo_noleap_precip.nc'#4back_era5_daily_noleap_precip.nc'#era5_daily_2mtemp.nc'#mjo_precip_local.nc'#'mjo_200_precip.nc'#small_2mtemp.nc'#tropic_200_z500.nc'
 
-var     = xr.open_dataset(load_dir+filename)['tp']#*86400#)[:,96:,80:241] #t2m  #pr #tp
-lats  = xr.open_dataset(load_dir+filename)['latitude'].values#[96:]
-lons   = xr.open_dataset(load_dir+filename)['longitude'].values#[80:241]
-time = xr.open_dataset(load_dir+filename)['time'].values[4:(365*60)+4]#[4:(365*60)+4]#[0:(365*60)+50+0]
+var     = xr.open_dataset(load_dir+filename)['tp']#[:,96:,80:241]#*86400#)[:,96:,80:241] #t2m  #pr #tp
+lats  = xr.open_dataset(load_dir+filename)['lat'].values#[96:]
+lons   = xr.open_dataset(load_dir+filename)['lon'].values#[80:241]
+time = xr.open_dataset(load_dir+filename)['time'].values[0:(365*60)+50+0]#[0:(365*60)+50+0]#[4:(365*60)+4]#[4:(365*60)+4]#[0:(365*60)+50+0]
 
 print('taking tropical mean...')
 print(var)
 # lat_avg_var = var.mean(dim = 'time', skipna = True) # skips the missing values - I think xarray does this by default
 
-
+# exit()
 # # time = lat_avg_var['time']
 
 #     #================================================================ 
 #     # REMOVE THE POLYNOMIAL TREND
 #     #================================================================ 
-# print('removing polynomial trend')
+# print('removing polynomial trend')FanfalonCoolest766
 # lat_avg_var = lat_avg_var.groupby('time.dayofyear').map(subtract_trend)
 
 # print(lat_avg_var)
 # print(lat_avg_var.shape)
 
-var_byday = var.groupby('time.dayofyear')
-#var_byday = var.groupby('time.dayofyear').map(subtract_trend)
+var_stacked = var.stack(z=('lat', 'lon'))
 
-var_anom = var_byday - var_byday.mean(dim='time')
+grouper = xr.DataArray(pd.MultiIndex.from_arrays([var_stacked.time.dt.month.values, var_stacked.time.dt.day.values],names=['month', 'day'],), dims=['time'], coords=[var_stacked.time],)
 
-print(var_anom)
-print(var_anom.shape)
+# for dum, dum1 in var_stacked.groupby(grouper):
 
-for lat in np.arange(0, lats.shape[0], 1):
-    print("next lat: " + str(lat))
-    for lon in np.arange(0, lons.shape[0], 1):
-        single_day = np.zeros(time.shape[0])
-        for day in np.arange(0,time.shape[0],1):
-            # print(day)
-            single_day[day] = var_anom[day][lat][lon]
-        print(single_day)
-        X = np.arange(0, single_day.shape[0], 1)
-        X = np.reshape(X, (X.shape[0], 1))
-        model = LinearRegression()
-        model.fit(X, single_day)
-        trend = model.predict(X)
-        for day in np.arange(0,time.shape[0],1):
-            # print(day)
-            var_anom[day][lat][lon] -= trend[day]
+    # if(dum[1] == 2 and dum[0] == 3):
+    #     print(dum1.time)
+    #     exit()
 
-var_c_anom = running_mean(var_anom.values, 5)
-var_c_anom = var_c_anom[0:(365*60)][:][:]#[0:(365*60)+50][:][:]
-print(var_c_anom)
-print(var_c_anom.shape)
+
+anom = []
+# for var_day in var_byday:
+for label, var_day in var_stacked.groupby(grouper):
+    # print(type(var_day.time.values[0]))
+    # print(var_day.time.dt.day)
+    # exit()
+    curve = polynomial.polyfit(np.arange(0,var_day.shape[0]),var_day,1)
+
+    trend = polynomial.polyval(np.arange(0,var_day.shape[0]),curve,tensor=True)
+
+    trend = np.swapaxes(trend,0,1)
+
+    diff  = var_day- trend
+    # print(trend.shape)
+    anom.append(diff)
+
+# print(anom)
+anom_xr_trend = xr.concat(anom,dim='time').unstack()
+anom_xr_trend = anom_xr_trend.sortby('time')
+# print(anom_xr_trend.shape)
+# print(anom_xr_trend)
+
+
+var_byday = anom_xr_trend.groupby(grouper)
+
+# print(var_byday.groups)
+# print(list(var_byday.groups.keys())[0])
+
+# print(var_day.time.values)
+# print(var_byday[list(var_byday.groups.keys())[70]])
+# print(var_byday[list(var_byday.groups.keys())[70]].time)
+# print("poop")
+
+print("wtf")
+
+# anom_xr_trend.rolling(5)
+rolled_trend = anom_xr_trend.rolling(time = 5).mean().dropna("time", how='all')#[0:365*60]
+print("finished")
+print(anom_xr_trend)
+print(rolled_trend)
+# var_c_anom = var_c_anom[0:(365*60)+50]#[0:(365*60)+50][:][:]
 
 #Make a dataarray and then make it a netcdf to save.
-df = xr.DataArray(var_c_anom, coords=[('time', time), ('lat', lat), ('lon', lon)], name='era5precipanom')
-
-#df.to_netcdf('/Users/nicojg/Documents/Work/2021_Fall_IAI/Code/TLLTT/data/era5_2mtemp_precip.nc')
+# df = xr.DataArray(var_c_anom, coords=[('time', time), ('lat', lats), ('lon', lons)], name='era5precipanom')
 
 
-df.to_netcdf('/ourdisk/hpc/ai2es/nicojg/TLLTT/data/era5_precip_mjo_notrend_anoms.nc')
+anom_xr_trend.to_netcdf('/barnes-scratch/nicojg/MAIN_era5_precip_mjo_notrend_anoms.nc')
+
+# alaska_lat = find_nearest_index(lats, 61.2176)
+# alaska_lon = find_nearest_index(lons, 360-149.8997)
+
+
+for var_day in anom_xr_trend.groupby('time.dayofyear'):
+    for lat_day in np.arange(0,var_day[1].shape[1],1):
+        for lon_day in np.arange(0,var_day[1].shape[2],1):
+            # print(np.mean(var_day[1][:,alaska_lat,alaska_lon]))
+            print(np.mean(var_day[1][:,0,0]))
+            exit()
+
+
+
+
+exit()
+
+anom = []
+print(np.where(var == var_byday[1][1])[0])
+
+for var_day in var_byday:
+    print(var_day[1].shape)
+    # print(var_day[0])
+    curve = polynomial.polyfit(np.arange(0,var_day[1].shape[0]),var_day[1],5)
+
+    trend = polynomial.polyval(np.arange(0,var_day[1].shape[0]),curve,tensor=True)
+
+    print(trend)
+
+    exit()
+
+# for var_day in var_byday:
+#     # print(var_day[0])
+#     for lat_day in np.arange(0,var_day[1].shape[1],1):
+#         for lon_day in np.arange(0,var_day[1].shape[2],1):
+#             curve = polynomial.polyfit(np.arange(0,var_day[1].shape[0]),var_day[1][:,lat_day,lon_day],1)
+
+#             trend = polynomial.polyval(np.arange(0,var_day[1].shape[0]),curve,tensor=True)
+
+#             diff = var_byday - trend
+#             anom.append(diff)
+
+            # for i in np
+            # print(curve)
+        
+#var_byday = var.groupby('time.dayofyear').map(subtract_trend)
+
+# for i in np.arange(1,366,1):
+#     print(var_byday[i].time.shape[0])
+
+# print(var_byday[120].time)
+# var_anom = var_byday - var_byday.mean(dim='time')
+
+# print(var_anom)
+# print(var_anom.shape)
+
+# for lat in np.arange(0, lats.shape[0], 1):
+#     print("next lat: " + str(lat))
+#     for lon in np.arange(0, lons.shape[0], 1):
+#         single_day = np.zeros(time.shape[0])
+#         for day in np.arange(0,time.shape[0],1):
+#             # print(day)
+#             single_day[day] = var_anom[day][lat][lon]
+#         print(single_day)
+#         X = np.arange(0, single_day.shape[0], 1)
+#         X = np.reshape(X, (X.shape[0], 1))
+#         model = LinearRegression()
+#         model.fit(X, single_day)
+#         trend = model.predict(X)
+#         for day in np.arange(0,time.shape[0],1):
+#             # print(day)
+#             var_anom[day][lat][lon] -= trend[day]
+
+# var_c_anom = running_mean(var_anom.values, 5)
+# var_c_anom = var_c_anom[0:(365*60)][:][:]#[0:(365*60)+50][:][:]
+# print(var_c_anom)
+# print(var_c_anom.shape)
+
+# #Make a dataarray and then make it a netcdf to save.
+# df = xr.DataArray(var_c_anom, coords=[('time', time), ('lat', lat), ('lon', lon)], name='era5precipanom')
+
+# df.to_netcdf('/barnes-scratch/nicojg/era5_precip_mjo_notrend_anoms.nc')
 
 exit()
 
--- preprocess_winter_ERA5.py
+++ preprocess_winter_ERA5.py
@@ -1,10 +1,9 @@
-import py3nvml
-py3nvml.grab_gpus(num_gpus=1, gpu_select=[3])
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
 from icecream import ic
 import scipy.io as sio
+import pandas as pd
 
 def running_mean(x, N):
     cumsum = np.cumsum(np.insert(x, 0, 0)) 
@@ -15,54 +14,67 @@ def find_nearest_index(array, value):
     idx = (np.abs(array - value)).argmin()
     return idx
 
-load_dir = "/ourdisk/hpc/ai2es/nicojg/TLLTT/data/"
+# load_dir = "/ourdisk/hpc/ai2es/nicojg/TLLTT/data/"
+load_dir = "/barnes-scratch/nicojg/"
 
 # filename = '500_2mtemp.nc'
 # temp_500     = xr.open_dataset(load_dir+filename)['tas'].values[:,96:,80:241]
 #filename = 'NA_2mtemp.nc'
-filename = 'era5_2mtemp_anoms.nc'#era5_daily_2mtemp.nc'
-temp     = xr.open_dataset(load_dir+filename)['era5tempanom'].values#[:,96:,80:241]
-time     = xr.open_dataset(load_dir+filename)['time'][0:(60*365)+14]
+filename = 'MAIN_era5_2mtemp_mjo_notrend_anoms.nc'#era5_daily_2mtemp.nc'
+temp     = xr.open_dataset(load_dir+filename)['t2m']#[:,96:,80:241]
+# time     = xr.open_dataset(load_dir+filename)['time'][0:(60*365)+14]
 lat  = xr.open_dataset(load_dir+filename)['lat'].values
 lon   = xr.open_dataset(load_dir+filename)['lon'].values
-print(time)
-print(lat)
-print(lon)
+# print(time)
+# print(lat)
+# print(lon)
 
 
 
 #Find Alaska point
 
 alaska_lat = find_nearest_index(lat, 61.2176)
-alaska_lon = find_nearest_index(lon, 149.8997)
+alaska_lon = find_nearest_index(lon, 360-149.8997)
 
-years = 60
+print(alaska_lat)
+print(alaska_lon)
 
-full_loc_temp = []
+lead = 14
+# print(temp.shift(time=lead))
+# rolled_temp = temp.rolling(time = 5).mean().dropna("time", how='all')
+rolled_temp = temp.rolling(time = 5).mean()
+rolled_temp = rolled_temp.shift(time=-4).dropna("time", how='all')
 
-for i in np.arange(0,(years*365)+14,1):
-    # temp_loc = []
+
+shift_rolled_temp = rolled_temp.sel(time=slice("1960-01-15", "2021-01-14"))
+
+print(shift_rolled_temp)
+
+loc_shift_rolled_temp = shift_rolled_temp[:,alaska_lat, alaska_lon]
+print(loc_shift_rolled_temp)
+# for i in np.arange(0,(years*365)+14,1): #18 or 14
+#     # temp_loc = []
     
-    # #Then loop through the number of specified years. 
-    # for j in np.arange(0,train_years,1):
+#     # #Then loop through the number of specified years. 
+#     # for j in np.arange(0,train_years,1):
 
-    #     #Add up the sea level pressure for that particular day from every year.
-    #     temp_loc.append(temp[int(i+(days*j)),46,136])
+#     #     #Add up the sea level pressure for that particular day from every year.
+#     #     temp_loc.append(temp[int(i+(days*j)),46,136])
 
-    #full_loc_temp.append(temp[i, 46,136] - avgtemp_day[i%365, 46,136]) #Madison
-    # full_loc_temp.append(temp[i, 52,110] - avgtemp_day[i%365, 52,110]) #Vancouver
-    # full_loc_temp.append(temp[i, 36,113] - avgtemp_day[i%365, 36,113]) #Los Angeles
-    # full_loc_temp.append(temp[i, loc_lat,loc_lon] - avgtemp_day[i%365, loc_lat,loc_lon])
-    full_loc_temp.append(temp[i, alaska_lat,alaska_lon])  #Anchorage
-    # full_loc_temp.append(temp[i, 44,109] - avgtemp_day[i%365, 44,109])  #Crescent City
+#     #full_loc_temp.append(temp[i, 46,136] - avgtemp_day[i%365, 46,136]) #Madison
+#     # full_loc_temp.append(temp[i, 52,110] - avgtemp_day[i%365, 52,110]) #Vancouver
+#     # full_loc_temp.append(temp[i, 36,113] - avgtemp_day[i%365, 36,113]) #Los Angeles
+#     # full_loc_temp.append(temp[i, loc_lat,loc_lon] - avgtemp_day[i%365, loc_lat,loc_lon])
+#     full_loc_temp.append(temp[i, alaska_lat,alaska_lon])  #Anchorage
+#     # full_loc_temp.append(temp[i, 44,109] - avgtemp_day[i%365, 44,109])  #Crescent City
 
 
-    # full_loc_temp[-1] = full_loc_temp[-1] - temp_200_years[i, 64, 88] #Remove past two year mean
+#     # full_loc_temp[-1] = full_loc_temp[-1] - temp_200_years[i, 64, 88] #Remove past two year mean
 
-print("Mean: " + str(np.mean(full_loc_temp)))
-print(len(full_loc_temp))
+# print("Mean: " + str(np.mean(full_loc_temp)))
+# print(len(full_loc_temp))
 
-#Only if wasnt done before!!!!
+# #Only if wasnt done before!!!!
 # full_loc_temp = running_mean(full_loc_temp, 5)
 
 
@@ -71,8 +83,155 @@ print(len(full_loc_temp))
 # df.to_netcdf('/Users/nicojg/Documents/Work/2021_Fall_IAI/Code/TLLTT/data/small_2mtemp_nocycle_5back.nc')
 # exit()
 
+year_range = np.arange(1960, 2021, 1)
 
-print(time)
+date_range = []
+for year in year_range:
+    if year == 1960:
+        date_range.append(pd.date_range(start= f'{year}-01-15',end = f'{year}-03-14',freq='d') + pd.offsets.Hour(11))
+        date_range.append(pd.date_range(start= f'{year}-11-15',end = f'{year+1}-03-14',freq='d') + pd.offsets.Hour(11))
+    elif year == 2020 :
+        date_range.append(pd.date_range(start= f'{year}-11-15',end = f'{year+1}-01-14',freq='d')+ pd.offsets.Hour(11))
+    else: 
+        date_range.append(pd.date_range(start= f'{year}-11-15',end = f'{year+1}-03-14',freq='d') + pd.offsets.Hour(11))
+        
+date_range = [item for sublist in date_range for item in sublist]
+# print(date_range)
+# print(loc_shift_rolled_temp)
+# print(loc_shift_rolled_temp.sel(time=date_range))
+
+loc_shift_rolled_temp = loc_shift_rolled_temp.sel(time=date_range)
+
+print(loc_shift_rolled_temp)
+
+lower = np.percentile(loc_shift_rolled_temp, 33.33)
+print("lower: " + str(lower))
+# upper = np.percentile(full_loc_temp_winter[14:full_loc_temp_winter.shape[0] - 32], 66.66)
+upper = np.percentile(loc_shift_rolled_temp, 66.66)
+print("upper: " + str(upper))
+
+
+train_dates = loc_shift_rolled_temp.sel(time=slice("1960-01-15", "2012-01-14"))
+print("train_dates: " + str(train_dates.shape[0]))
+
+dum_test = loc_shift_rolled_temp.sel(time=slice("1960-01-15", "1962-01-14"))
+print("dum_test: " + str(dum_test.shape[0]))
+
+train_class = []
+
+for i in np.arange(0, train_dates.shape[0], 1):
+
+    #CHANGE
+
+    # correct_day = (i+lead)%365
+    # true_slp = temp[i+lead,46,136] - avgtemp_day[correct_day,46,136]
+
+    # true_temps.append(true_slp)
+    true_slp = loc_shift_rolled_temp[i]
+
+    # print("true_slp: " + str(true_slp))
+    if((true_slp < lower)):
+        train_class.append(0)
+    elif((true_slp > upper)):
+        train_class.append(2)
+    else:
+        train_class.append(1)
+
+    # if((true_slp < mid)):
+    #     train_class.append(0)
+    # else:
+        #     train_class.append(1)
+
+count_arr = np.bincount(train_class)
+
+print("number of 0: " + str(count_arr[0]))
+print("number of 1: " + str(count_arr[1]))
+print("number of 2: " + str(count_arr[2]))
+
+for i in np.arange(train_dates.shape[0], loc_shift_rolled_temp.shape[0], 1):
+
+    #CHANGE
+
+    # correct_day = (i+lead)%365
+    # true_slp = temp[i+lead,46,136] - avgtemp_day[correct_day,46,136]
+
+    # true_temps.append(true_slp)
+    true_slp = loc_shift_rolled_temp[i]
+
+    # print(true_slp)
+    # print(full_loc_temp_useful_days[i])
+
+    # print("true_slp: " + str(true_slp))
+    if((true_slp < lower)):
+        train_class.append(0)
+    elif((true_slp > upper)):
+        train_class.append(2)
+    else:
+        train_class.append(1)
+
+count_arr = np.bincount(train_class)
+
+print("number of 0: " + str(count_arr[0]))
+print("number of 1: " + str(count_arr[1]))
+print("number of 2: " + str(count_arr[2]))
+
+print(len(train_class))
+
+np.savetxt('/barnes-scratch/nicojg/ERA5_winter_ternary_alaska_points_5days.txt', train_class, fmt='%d')
+
+y_predict_class_plot = np.asarray(train_class)
+
+y_predict_class_plot_low = y_predict_class_plot.astype('float64')
+y_predict_class_plot_avg = y_predict_class_plot.astype('float64')
+y_predict_class_plot_high = y_predict_class_plot.astype('float64')
+
+y_predict_class_plot_low[np.where(y_predict_class_plot_low==2)[0]] = np.nan
+y_predict_class_plot_low[np.where(y_predict_class_plot_low==1)[0]] = np.nan
+y_predict_class_plot_avg[np.where(y_predict_class_plot_avg==2)[0]] = np.nan
+y_predict_class_plot_avg[np.where(y_predict_class_plot_avg==0)[0]] = np.nan
+
+y_predict_class_plot_high[np.where(y_predict_class_plot_high==1)[0]] = np.nan
+y_predict_class_plot_high[np.where(y_predict_class_plot_high==0)[0]] = np.nan
+
+
+plt.figure(figsize=(20,6))
+plt.scatter(np.arange(1,len(y_predict_class_plot_low)+1,1)/120,y_predict_class_plot_low, s=1 )
+plt.scatter(np.arange(1,len(y_predict_class_plot_avg)+1,1)/120,y_predict_class_plot_avg, s=1 )
+plt.scatter(np.arange(1,len(y_predict_class_plot_high)+1,1)/120,y_predict_class_plot_high, s=1 )
+plt.yticks([0,1,2])
+plt.title("Label Class", fontsize=20)
+plt.xlabel("Year", fontsize=15)
+plt.ylabel("Class", fontsize=15)
+plt.savefig(("../Figures" + "/timeseries/" + 'ERA5_WINTER_FULL_timeseries_mjo.png'), bbox_inches='tight')
+
+for half_decade in np.arange(5,65,5):
+    plt.figure(figsize=(20,6))
+    plt.scatter(np.arange(1,len(y_predict_class_plot_low)+1,1)/120,y_predict_class_plot_low, s=1 )
+    plt.scatter(np.arange(1,len(y_predict_class_plot_avg)+1,1)/120,y_predict_class_plot_avg, s=1 )
+    plt.scatter(np.arange(1,len(y_predict_class_plot_high)+1,1)/120,y_predict_class_plot_high, s=1 )
+    plt.yticks([0,1,2])
+    plt.title("Label Class by half decades", fontsize=20)
+    plt.xlabel("Year", fontsize=15)
+    plt.ylabel("Class", fontsize=15)
+    plt.xlim(half_decade-5,half_decade)
+    plt.savefig(("../Figures" + "/timeseries/" + 'ERA5_WINTER_halfdecade_timeseries_' + str(half_decade)+ '_mjo.png'), bbox_inches='tight')
+    # plt.show()
+
+for decade in np.arange(10,70,10):
+    plt.figure(figsize=(20,6))
+    plt.scatter(np.arange(1,len(y_predict_class_plot_low)+1,1)/120,y_predict_class_plot_low, s=1 )
+    plt.scatter(np.arange(1,len(y_predict_class_plot_avg)+1,1)/120,y_predict_class_plot_avg, s=1 )
+    plt.scatter(np.arange(1,len(y_predict_class_plot_high)+1,1)/120,y_predict_class_plot_high, s=1 )
+    plt.yticks([0,1,2])
+    plt.title("Label Class by decades", fontsize=20)
+    plt.xlabel("Year", fontsize=15)
+    plt.ylabel("Class", fontsize=15)
+    plt.xlim(decade-10,decade)
+    plt.savefig(("../Figures" + "/timeseries/" + 'ERA5_WINTER_decade_timeseries_' + str(decade)+ '_mjo.png'), bbox_inches='tight')
+    # plt.show()
+
+quit()
+
 all_months = time["time.month"].values
 
 all_days = time["time.day"].values
@@ -106,31 +265,7 @@ full_loc_temp_useful_days = np.asarray(full_loc_temp_useful_days[14:len(full_loc
 
 print("useful days: " + str(full_loc_temp_useful_days.shape))
 
-filename = 'small_2mtemp_200_winter.nc'
-time     = xr.open_dataset(load_dir+filename)['time']
-
-new_time = time[:time.shape[0] - 4]
-
-print("new_time shape: " + str(new_time.shape))
-
-new_months = new_time["time.month"].values
-
-new_years = time["time.year"].values
-
-
-years = np.unique(new_years)
-
-years_train = years[:45]
-
-years_train = np.isin(new_years, years_train)
-
-iyears_train = np.where(years_train==True)[0]
-
-num_of_train_days = iyears_train.shape[0]
-
-# full_loc_temp = np.asarray(full_loc_temp).flatten()
-
-train_years = 45
+train_years = 52
 test_years = 60
 
 train_class = [] 
@@ -151,7 +286,6 @@ mid = np.percentile(full_loc_temp_useful_days, 50.)
 
 print(full_loc_temp_winter.shape[0] - 46)
 
-print(new_months.shape)
 for i in np.arange(0, 5400, 1):
 
     #CHANGE
@@ -218,8 +352,12 @@ print("number of 2: " + str(count_arr[2]))
 # np.savetxt('/Users/nicojg/Documents/Work/2021_Fall_IAI/Code/TLLTT/data/temp_200years_threedays_test.txt', true_temps)
 
 
-np.savetxt('/ourdisk/hpc/ai2es/nicojg/TLLTT/data/ERA5_winter_ternary_alaska_points.txt', train_class, fmt='%d')
+# np.savetxt('/ourdisk/hpc/ai2es/nicojg/TLLTT/data/ERA5_winter_ternary_alaska_points.txt', train_class, fmt='%d')
+print(len(train_class))
 
+np.savetxt('/barnes-scratch/nicojg/ERA5_winter_ternary_alaska_points_7days.txt', train_class, fmt='%d')
+
+print(temp[0,alaska_lat, alaska_lon])
 # y_predict_class_plot = np.asarray(train_class)
 
 # y_predict_class_plot_low = y_predict_class_plot.astype('float64')
